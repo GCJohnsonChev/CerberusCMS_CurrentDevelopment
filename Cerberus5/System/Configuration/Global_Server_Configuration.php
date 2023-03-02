4edee83bbe132d58572330f40ab49f2aa1f498d8
Gary Christopher Johnson <47867437+GCJohnsonChev@users.noreply.github.com>
2023-03-02 09:24:43 -0800

    Add files via upload

---
+++ Cerberus5/System/Configuration/Global_Server_Configuration.php
@@ -0,0 +1,65 @@
+<?php
+/*
+ ============================================================================================================
+ + Cerberus Content Management System
+ + ---
+ + - Author 		     : Gary Christopher Johnson - Rosedale, California
+ + - Electronic Mail Address : TinkeSoftware@Protonmail.com, CerberusCMS@Protonmail.com
+ + - Company		     : Tinke Software
+ + - Company Address	     : Rosedale, California, U.S.A.
+ + - Document Notes	     : View this file in a non-formatting text editor without word-wrap for the correct
+ +			       display of this programming code and its indentation.
+ + ---
+ +
+ +
+ +
+ +
+ +
+ +
+ +
+ +
+ +
+ +
+ +
+ + ---
+ + - File Location: Master Root Directory->Root Directory->System->Configuration->Global_Server_Configuration.php
+ + - File Version : 0.7 - Pure Pre-Hyper-Text-Post-Processor
+ + - Release Date : Saturday, April 8th of 2023
+ + ---
+ + -------------------------------------------------------------------------------
+ + --[][]--[][][]--[][][]--[][][]---[][][]--[][][]--[]--[]------[][][]------------
+ + -[]-----[]------[]--[]--[]---[]--[]------[]--[]--[]--[]------[]----------------
+ + -[]-----[]------[]--[]--[]---[]--[]------[]--[]--[]--[]------[]----------------
+ + -[]-----[]------[]--[]--[]---[]--[]------[]--[]--[]--[]------[]----------------
+ + -[]-----[][][]--[][][]--[][][]---[][][]--[][][]--[]--[]------[]----------------
+ + -[]-----[]------[]--[]--[]---[]--[]------[]--[]--[]--[]------[]----------------
+ + -[]-----[]------[]--[]--[]---[]--[]------[]--[]--[]--[]------[]------------/-\-
+ + -[]-----[]------[]--[]--[]---[]--[]------[]--[]--[]--[]------[]------------|4|- ~ Artemis ~
+ + --[][]--[][][]--[]--[]--[][][]---[][][]--[]--[]--[][][]--[][][]------------\-/- Build Number: 0.8
+ + ------------------------------------------------------------------------------- Edit Number : 1,954
+ ============================================================================================================
+*/
+
+/*
+ ============================================================================================================
+ +
+ +
+ + Cerberus :: System Configuration :: Global Server Configuration
+ +
+ +
+ ============================================================================================================
+*/
+
+$_ACCESS_DATABASE_SERVER_HOSTNAME			= "";
+$_ACCESS_DATABASE_SERVER_USERNAME			= "";
+$_ACCESS_DATABASE_SERVER_PASSWORD			= "";
+$_ACCESS_DATABASE_SERVER_DATABASE_NAME			= "";
+$_ACCESS_DATABASE_SERVER_DATABASE_TABLE_PREFIX		= "";
+$_ACCESS_PHP_ENGINE_VERSION				= "5";
+$_ACCESS_URL_CLEARTEXT					= "";
+$_ACCESS_URL_SECURE					= "";
+$_ACCESS_URL_SECURE_SSL					= "";
+$_ACCESS_URL_SECURE_OPENSSL				= "";
+$_ACCESS_URL_SECURE_TLS					= "";
+$_ACCESS_SYSTEM_ELECTRONIC_MAIL_ADDRESS			= "";
+?>
